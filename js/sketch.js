0d9141be769b466ec966b7430c4cf98adc3a0e93
cgusb <cgusbecker@gmail.com>
2022-05-16 09:28:03 -0400

    Replace 'reset()' with custom 'resetSketch()'

--- js/sketch.js
+++ js/sketch.js
@@ -1,8 +1,13 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 400)
+  resetSketch()
   console.log('Press SPACE to stop looping or r to reset.')
 }
 
 function draw() {
-  background(220);
+  background(220)
+}
+
+function resetSketch() {
+  background(220)
 }
